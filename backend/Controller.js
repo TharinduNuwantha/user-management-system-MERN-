9449856bd0b55bdc1301306c3d1262aa630e8e8c
Tharindu Nuwantha <105153249+TharinduNuwantha@users.noreply.github.com>
2025-08-26 17:09:46 +0530

    Error Fixed - API woring

--- backend/Controller.js
+++ backend/Controller.js
@@ -17,31 +17,34 @@ const getUsers = (req,res,next) =>{
         
 }
 
-const addUser = (req,res,next)=>{
-    const user = new User({
-        id:req.body.id,
-        name:req.body.name
-    })
-    user.save()
-        .then(Response => {
-            res.json({response});
-        })
-        .catch((error) => {
-            res.json({error});
-        })
-}
+const addUser = (req, res, next) => {
+  const user = new User({
+    id: req.body.id,
+    name: req.body.name
+  });
 
-const updateUser = (req,res,next) =>{
-    const {id,name} = req.body;
-    User.updateOne({id:id},{$set:{name:name}}
+  user.save()
+    .then(response => {
+      res.json(response);  
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.message });
+    });
+};
+
+
+const updateUser = (req, res, next) => {
+    const { id, name } = req.body;
+
+    User.updateOne({ id: id }, { $set: { name: name } })
         .then(response => {
-            res.json({response});
+            res.json(response);
         })
         .catch(error => {
-            res.json({error});
-        })
-    )
+            res.status(500).json({ error: error.message });
+        });
 }
+
 
 const deleteUser = (req,res,next) =>{
     const id = req.body.id;
